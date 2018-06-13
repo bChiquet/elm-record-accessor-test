85e74d12e319c98dbeab38c43454f3a9b61a5d7a
bec <benoit.chiquet@gmail.com>
2018-06-13 16:05:27 +0200

    Lenses v2 : type inference problem.

--- gen/Lens.elm
+++ gen/Lens.elm
@@ -4,63 +4,18 @@ import LensTypes exposing (Accessor(..))
 
 
 r = {
-    bar = \sub -> \super ->
-      let subAccessor = sub super.bar in
-      Accessor { get  = subAccessor.get
-               , over = \f -> { super | bar = subAccessor.over f }
+    bar = \(Accessor sub) ->
+      Accessor { get  = \b -> sub.get b.bar
+               , over = \f -> \b -> { b | bar = sub.over f b.bar }
                }
     ,
-    com = \sub -> \super ->
-      let subAccessor = sub super.com in
-      Accessor { get  = subAccessor.get
-               , over = \f -> { super | com = subAccessor.over f }
+    foo = \(Accessor sub) ->
+      Accessor { get  = \b -> sub.get b.foo
+               , over = \f -> \b -> { b | foo = sub.over f b.foo}
                }
     ,
-    elm = \sub -> \super ->
-      let subAccessor = sub super.elm in
-      Accessor { get  = subAccessor.get
-               , over = \f -> { super | elm = subAccessor.over f }
-               }
-    ,
-    field = \sub -> \super ->
-      let subAccessor = sub super.field in
-      Accessor { get  = subAccessor.get
-               , over = \f -> { super | field = subAccessor.over f }
-               }
-    ,
-    foo = \sub -> \super ->
-      let subAccessor = sub super.foo in
-      Accessor { get  = subAccessor.get
-               , over = \f -> { super | foo = subAccessor.over f }
-               }
-    ,
-    get = \sub -> \super ->
-      let subAccessor = sub super.get in
-      Accessor { get  = subAccessor.get
-               , over = \f -> { super | get = subAccessor.over f }
-               }
-    ,
-    map = \sub -> \super ->
-      let subAccessor = sub super.map in
-      Accessor { get  = subAccessor.get
-               , over = \f -> { super | map = subAccessor.over f }
-               }
-    ,
-    md = \sub -> \super ->
-      let subAccessor = sub super.md in
-      Accessor { get  = subAccessor.get
-               , over = \f -> { super | md = subAccessor.over f }
-               }
-    ,
-    over = \sub -> \super ->
-      let subAccessor = sub super.over in
-      Accessor { get  = subAccessor.get
-               , over = \f -> { super | over = subAccessor.over f }
-               }
-    ,
-    qux = \sub -> \super ->
-      let subAccessor = sub super.qux in
-      Accessor { get  = subAccessor.get
-               , over = \f -> { super | qux = subAccessor.over f }
+    qux = \(Accessor sub) ->
+      Accessor { get  = \b -> sub.get b.qux
+               , over = \f -> \b -> { b | qux = sub.over f b.qux}
                }
     }
--- gen/LensTypes.elm
+++ gen/LensTypes.elm
@@ -1,83 +1,29 @@
 module LensTypes exposing (Accessor(Accessor), get)
 
 type Accessor super sub = 
-  Accessor { get : sub
-           , over : (sub -> sub) -> super }
+  Accessor { get : super -> sub 
+           , over : (sub -> sub) -> (super -> super) }
 
--- We can't use this signature because Elm has no rank N types.
--- But don't worry! It only means that we will have longer signatures...
--- The shorter signatures will be provided in comments.
+--fieldLens : Accessor a i -> Accessor {b | field : a} i
+--fieldLens (Accessor sub) = 
+--  Accessor { get  = \b -> sub.get b.field
+--           , over = \change -> (\b -> {b | field = sub.over change b.field })
+--           }
 
---type alias Lens super sub = (sub   -> Accessor sub   a)
---                         -> (super -> Accessor super a)
-
---fieldLens : Lens {b | field : a} a
-fieldLens sub = \super -> 
-  let subAccessor = sub super.field in
-  Accessor { get  = subAccessor.get
-           , over = \f -> { super | field = subAccessor.over f }
-           }
-
-----onEach : Lens (List a) a
---onEach sub = \list ->
---  let subAccessor = List.map sub list in
---  Accessor { get  = List.map .get subAccessor
---           , over = \f -> List.map (.over f) subAccessor
+--onEach : Accessor a i -> Accessor (List a) i
+--onEach (Accessor sub) =
+--  Accessor { get  = \b -> List.map sub.get b
+--           , over = \change -> (\b -> List.map (sub.over change) b)
 --           }
 
 
-{- 
-
--- TYPE MISMATCH ----------------------------------------- ././gen/LensTypes.elm
-
-The argument to function `lens` is causing a mismatch.
-
-38|                   lens id)
-                           ^^
-                           Function `lens` is expecting the argument to be:
-
-                               a -> Accessor b a
-
-                               But it is:
-
-                                   a -> Accessor a a
-
-                                   Hint: Your type annotation uses `b` and `a`
-                                   as separate type variables. Each one
-                                   can be any type of value, but because they
-                                   have separate names, there is no
-                                   guarantee that they are equal to each other.
-                                   Your code seems to be saying they
-                                   ARE the same though! Maybe they should be the
-                                   same in your type annotation?
-                                   Maybe your code uses them in a weird way?
-                                   More at:
-                                   <https://github.com/elm-lang/elm-compiler/blob/0.18.0/hints/type-annotations.md>
-
-                                   Detected errors in 1 module.                                        
-
--}
---Apparently this error happens because of declaring declaring id. 
-
-id : z -> Accessor z z
-id = \z ->
-  Accessor { get  = z
-           , over = \f -> f z
+--id : Accessor z z
+id =
+  Accessor { get  = \a -> a
+           , over = \change -> (\a -> change a)
            }
 
---get : Lens s a -> s -> a
---get : ((a -> Accessor a i) -> (b -> Accessor b i)) -> b -> i
+--get : ( Accessor a i -> Accessor b i) -> b -> i
 get lens s = 
   let accessor = (lens id) in
-  accessor.get
-
---set : Lens s a -> a -> s -> s
-set lens a s = 
-  let accessor = (lens id) in
-  accessor.over (\_ -> a) s
-
---over : Lens s a -> (a -> a) -> s -> s
-over lens f s =
-  let accessor = (lens id) in
-  accessor.over f s
-
+  accessor.get s
