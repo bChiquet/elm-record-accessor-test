b7e09f20f8971ae6b71511d7ef6f51f67952dbd3
bec <benoit.chiquet@gmail.com>
2018-06-13 17:09:40 +0200

    nested examples

--- tests/Spec.elm
+++ tests/Spec.elm
@@ -7,7 +7,8 @@ import LensTypes exposing (get, set, over)
 import Lens exposing (r)
 
 
-exampleRecord = {foo = 3, bar = "Yop", qux = False}
+simpleRecord = {foo = 3, bar = "Yop", qux = False}
+nestedRecord = {foo = simpleRecord}
 
 suite : Test
 suite =
@@ -15,24 +16,39 @@ suite =
     [ describe "get"
       [ test "simple get" <| \_ -> 
           Expect.equal 
-            ( get r.foo exampleRecord)
+            ( get r.foo simpleRecord)
             3
+      , test "nested get" <| \_ ->
+          Expect.equal
+            (get (r.foo << r.bar) nestedRecord)
+            "Yop"
       ]
-
     , describe "set"
       [ test "simple set" <| \_ ->
           let updatedExample = 
-            (set r.qux True exampleRecord)
+            (set r.qux True simpleRecord)
           in Expect.equal
             updatedExample.qux
             True
+      , test "nested set" <| \_->
+          let updatedExample = 
+            (set (r.foo << r.foo) 5 nestedRecord)
+          in Expect.equal
+            updatedExample.foo.foo
+            5
       ]
     , describe "over"
       [ test "simple over" <| \_ ->
           let updatedExample =
-            (over r.bar (\w -> w ++ " lait") exampleRecord)
+            (over r.bar (\w -> w ++ " lait") simpleRecord)
           in Expect.equal
             updatedExample.bar
             "Yop lait"
+      , test "nested over" <| \_ ->
+          let updatedExample =
+            (over (r.foo << r.qux) (\w -> not w) nestedRecord)
+          in Expect.equal
+            updatedExample.foo.qux
+            True
       ]
     ]
